0cb2af5c635bd051f595b35240f906f6a57960dc
Juhi Damley <juhidamley@hotmail.com>
2026-07-19 19:33:56 -0700

    Initial Commit

---
+++ WakeMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeMe
+//
+//  Created by Juhi Damley on 7/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeMe/WakeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeMeApp.swift
+//  WakeMe
+//
+//  Created by Juhi Damley on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
